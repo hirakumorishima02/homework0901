4f7bcadffae0dd6f798f202f757ecb92b0bea923
hirakumorishima02 <business.ujinchu@gmail.com>
2019-09-03 12:48:08 +0900

    reflect of indication no492

--- no492/index.php
+++ no492/index.php
@@ -12,20 +12,20 @@ $end = '2015-12-31';
 for ($i = $start; $i <= $end; $i = date('Y-m-d', strtotime($i . '+1 day'))) {
     list($y, $m, $d) = explode('-', $i);
     // もし1桁の日付であれば
-    // imo
+    // imfo
     // 比較の際も、半角スペースあけるとよみやすいです
     if (mb_strlen($d) === 1) {
         // ask
         // 条件と内容が逆ですかね？
-        // もし日付と月が異なれば
+        // もし月と日付が異なれば
         if ($m == $d) {
-            echo $i . PHP_EOL;
+            echo $i, PHP_EOL;
         }
     // もし2桁の日付であれば
     } elseif (mb_strlen($d) === 2) {
         // もし2桁の日付であれば
         if ($m == substr($d, 0, 1) + substr($d, 1, 1)) {
-            echo $i . PHP_EOL;
+            echo $i, PHP_EOL;
         }
     }
 }
